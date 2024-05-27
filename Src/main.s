924f06fa2d8f5989060b590861f1466cd3bb4294
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-05-27 13:10:51 -0600

    comments

--- Src/main.s
+++ Src/main.s
@@ -1,99 +1,143 @@
+/**
+ ******************************************************************************
+ * @file      startup_stm32f446retx.s
+ * @author    Dilary Sarahí Cruz López - 231010
+ *            Diego José López Campos - 23242
+ *            Jonathan Josúe Zacarías Bances - 231104
+ * @brief     Vector de interrupciones del dispositivo STM32F446RETx para el
+ *            conjunto de herramientas GCC.
+ *            Este módulo realiza:
+ *                - Configuración del SP inicial
+ *                - Configuración del PC inicial == Reset_Handler
+ *                - Configuración de las entradas de la tabla de vectores con
+ *                  las direcciones de las ISR de excepciones
+ *                - Salto a la función principal en la biblioteca C (que
+ *                  eventualmente llama a main()).
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * Todos los derechos reservados.
+ *
+ * Este software está licenciado bajo los términos que se pueden encontrar en el
+ * archivo LICENSE en el directorio raíz de este componente de software. Si no
+ * se proporciona un archivo LICENSE con este software, se proporciona TAL CUAL.
+ *
+ ******************************************************************************
+ *
+ * Universidad del Valle de Guatemala
+ * Facultad de Ingeniería
+ * Departamento de Ciencias de la Computación
+ * CC3054 Organización de computadoras y Assembler
+ * Ciclo 1 - 2024
+ * Proyecto 4
+ *
+ * Objetivo:
+ * Diseñar y programar el funcionamiento de los puertos GPIO del STM32, para un
+ * display de 7 segmentos. Este tendrá un comportamiento de activación/desactivación
+ * que realice el despliegue secuencial de las letras A, B, C, D, E, F, G, H.
+ * El corrimiento cíclico se activará al presionar un push button A. La señal de
+ * reloj para el control del circuito tendrá un período de 1.5 segundos.
+ *
+ * El funcionamiento debe ser cíclico, es decir, al finalizar la secuencia, esta
+ * debe iniciar de nuevo desde el estado 0.
+ ******************************************************************************
+ */
+/*--------------------------------------------------
+DEFINICIONES PARA HABILITAR EL RELOJ AHB1 PARA LOS PUERTOS A Y C
+--------------------------------------------------*/
+.equ    RCC_BASE,        0x40023800        // Dirección base del RCC
+.equ    AHB1ENR_OFFSET,  0x30              // Desplazamiento del registro de habilitación AHB1
+.equ    RCC_AHB1ENR,     (RCC_BASE + AHB1ENR_OFFSET) // Dirección del registro de habilitación AHB1 del RCC
+
+.equ    GPIOC_BASE,      0x40020800        // Dirección base del GPIOC
+.equ    GPIOA_BASE,      0x40020000        // Dirección base del GPIOA
+.equ    GPIOA_EN,        (1 << 0)          // Habilitar reloj para GPIOA (establecer bit 0)
+.equ    GPIOC_EN,        (1 << 2)          // Habilitar reloj para GPIOC (establecer bit 1)
+
+/*--------------------------------------------------
+DEFINICIONES PARA CONFIGURAR LOS PUERTOS A Y C
+--------------------------------------------------*/
+.equ    GPIOA_MODER_OFFSET, 0x00           // Desplazamiento del registro de modo de GPIOA
+.equ    GPIOC_MODER_OFFSET, 0x00           // Desplazamiento del registro de modo de GPIOC
+.equ    GPIOA_MODER,     (GPIOA_BASE + GPIOA_MODER_OFFSET) // Dirección del registro de modo de GPIOA
+.equ    GPIOC_MODER,     (GPIOC_BASE + GPIOC_MODER_OFFSET) // Dirección del registro de modo de GPIOC
+.equ    MODERPA0_OUT,    (1 << 0)          // Configurar PA0 como salida
+.equ    MODERPA1_OUT,    (1 << 2)          // Configurar PA1 como salida
+.equ    MODERPA4_OUT,    (1 << 8)          // Configurar PA4 como salida
+.equ    MODERPA6_OUT,    (1 << 12)         // Configurar PA6 como salida
+.equ    MODERPA7_OUT,    (1 << 14)         // Configurar PA7 como salida
+.equ    MODERPA8_OUT,    (1 << 16)         // Configurar PA8 como salida
+.equ    MODERPA9_OUT,    (1 << 18)         // Configurar PA9 como salida
+.equ    MODERPC13_IN,    (0 << 26)         // Configurar PC13 como entrada
+
+/*--------------------------------------------------
+DEFINICIONES PARA CONTROLAR LOS PUERTOS
+--------------------------------------------------*/
+.equ    GPIOA_ODR_OFFSET, 0x14             // Desplazamiento del registro de datos de salida de GPIOA
+.equ    GPIOC_IDR_OFFSET, 0x10             // Desplazamiento del registro de datos de entrada de GPIOC
+.equ    GPIOA_ODR,       (GPIOA_BASE + GPIOA_ODR_OFFSET) // Dirección del registro de datos de salida de GPIOA
+.equ    GPIOC_IDR,       (GPIOC_BASE + GPIOC_IDR_OFFSET) // Dirección del registro de datos de entrada de GPIOC
+
+.equ    LEDPA0_ON,       (1 << 0)          // Encender PA0 (encender LED)
+.equ    LEDPA1_ON,       (1 << 1)          // Encender PA1 (encender LED)
+.equ    LEDPA4_ON,       (1 << 4)          // Encender PA4 (encender LED)
+.equ    LEDPA6_ON,       (1 << 6)          // Encender PA6 (encender LED)
+.equ    LEDPA7_ON,       (1 << 7)          // Encender PA7 (encender LED)
+.equ    LEDPA8_ON,       (1 << 8)          // Encender PA8 (encender LED)
+.equ    LEDPA9_ON,       (1 << 9)          // Encender PA9 (encender LED)
+.equ    BUTTON_PC13,     (1 << 13)         // Máscara de bits para el botón PC13
+
 .syntax unified
 .cpu cortex-m4
 .fpu softvfp
 .thumb
-
-/*--------------------------------------------------
-DEFINITIONS FOR AHB1 CLOCK ENABLE FOR PORT A AND PORT C
--------------------------------------------------*/
-.equ    RCC_BASE,        0x40023800        // Base address for RCC
-.equ    AHB1ENR_OFFSET,  0x30              // Offset for AHB1 enable register
-.equ    RCC_AHB1ENR,     (RCC_BASE + AHB1ENR_OFFSET) // RCC AHB1 enable register address
-
-.equ    GPIOC_BASE,      0x40020800        // Base address for GPIOC
-.equ    GPIOA_BASE,      0x40020000        // Base address for GPIOA
-.equ    GPIOA_EN,        (1 << 0)          // Enable clock for GPIOA (set bit 0)
-.equ    GPIOC_EN,        (1 << 2)          // Enable clock for GPIOC (set bit 1)
-
-/*--------------------------------------------------
-DEFINITIONS FOR CONFIGURING PORTS A AND C
--------------------------------------------------*/
-.equ    GPIOA_MODER_OFFSET, 0x00           // Offset for GPIOA mode register
-.equ    GPIOC_MODER_OFFSET, 0x00           // Offset for GPIOC mode register
-.equ    GPIOA_MODER,     (GPIOA_BASE + GPIOA_MODER_OFFSET) // GPIOA mode register address
-.equ    GPIOC_MODER,     (GPIOC_BASE + GPIOC_MODER_OFFSET) // GPIOC mode register address
-.equ    MODERPA0_OUT,    (1 << 0)          // Set PA0 as output
-.equ    MODERPA1_OUT,    (1 << 2)          // Set PA1 as output
-.equ    MODERPA4_OUT,    (1 << 8)          // Set PA4 as output
-.equ    MODERPA6_OUT,    (1 << 12)         // Set PA6 as output
-.equ    MODERPA7_OUT,    (1 << 14)         // Set PA7 as output
-.equ    MODERPA8_OUT,    (1 << 16)         // Set PA8 as output
-.equ    MODERPA9_OUT,    (1 << 18)         // Set PA9 as output
-.equ    MODERPC13_IN,    (0 << 26)         // Set PC13 as input
-
-/*--------------------------------------------------
-DEFINITIONS FOR CONTROLLING PORTS
--------------------------------------------------*/
-.equ    GPIOA_ODR_OFFSET, 0x14             // Offset for GPIOA output data register
-.equ    GPIOC_IDR_OFFSET, 0x10             // Offset for GPIOC input data register
-.equ    GPIOA_ODR,       (GPIOA_BASE + GPIOA_ODR_OFFSET) // GPIOA output data register address
-.equ    GPIOC_IDR,       (GPIOC_BASE + GPIOC_IDR_OFFSET) // GPIOC input data register address
-
-.equ    LEDPA0_ON,       (1 << 0)          // Set PA0 high (turn on LED)
-.equ    LEDPA1_ON,       (1 << 1)          // Set PA1 high (turn on LED)
-.equ    LEDPA4_ON,       (1 << 4)          // Set PA4 high (turn on LED)
-.equ    LEDPA6_ON,       (1 << 6)          // Set PA6 high (turn on LED)
-.equ    LEDPA7_ON,       (1 << 7)          // Set PA7 high (turn on LED)
-.equ    LEDPA8_ON,       (1 << 8)          // Set PA8 high (turn on LED)
-.equ    LEDPA9_ON,       (1 << 9)          // Set PA9 high (turn on LED)
-.equ    BUTTON_PC13,     (1 << 13)         // Bit mask for PC13 button
-
 .section .text
 .globl __main
 
 __main:
-    // Enable clock access to GPIOA and GPIOC
+    // Habilitar acceso al reloj para GPIOA y GPIOC
 
-    // Load address of RCC_AHB1ENR to r0
+    // Cargar la dirección de RCC_AHB1ENR en r0
     ldr r0, =RCC_AHB1ENR
-    // Load value at address found in r0 into r1
+    // Cargar el valor en la dirección encontrada en r0 en r1
     ldr r1, [r0]
-    // Enable clock for GPIOA and GPIOC
+    // Habilitar reloj para GPIOA y GPIOC
     orr r1, #GPIOA_EN
     orr r1, #GPIOC_EN
-    // Store content in r1 at address found in r0
+    // Almacenar el contenido en r1 en la dirección encontrada en r0
     str r1, [r0]
 
-    // Set PA0, PA1, PA4, PA6, PA7, PA8, PA9 as output
-    // Load address of GPIOA_MODER to r0
+    // Configurar PA0, PA1, PA4, PA6, PA7, PA8, PA9 como salida
+    // Cargar la dirección de GPIOA_MODER en r0
     ldr r0, =GPIOA_MODER
-    // Load value at address found in r0 into r1
+    // Cargar el valor en la dirección encontrada en r0 en r1
     ldr r1, [r0]
-    // Set PA0 as output
+    // Configurar PA0 como salida
     orr r1, #MODERPA0_OUT
-    // Set PA1 as output
+    // Configurar PA1 como salida
     orr r1, #MODERPA1_OUT
-    // Set PA4 as output
+    // Configurar PA4 como salida
     orr r1, #MODERPA4_OUT
-    // Set PA6 as output
+    // Configurar PA6 como salida
     orr r1, #MODERPA6_OUT
-    // Set PA7 as output
+    // Configurar PA7 como salida
     orr r1, #MODERPA7_OUT
-    // Set PA8 as output
+    // Configurar PA8 como salida
     orr r1, #MODERPA8_OUT
-    // Set PA9 as output
+    // Configurar PA9 como salida
     orr r1, #MODERPA9_OUT
-    // Store content in r1 at address found in r0
+    // Almacenar el contenido en r1 en la dirección encontrada en r0
     str r1, [r0]
 
-    // Set PC13 as input
-    // Load address of GPIOC_MODER to r0
+    // Configurar PC13 como entrada
+    // Cargar la dirección de GPIOC_MODER en r0
     ldr r0, =GPIOC_MODER
-    // Load value at address found in r0 into r1
+    // Cargar el valor en la dirección encontrada en r0 en r1
     ldr r1, [r0]
-    // Clear bits to set PC13 as input
+    // Borrar bits para configurar PC13 como entrada
     BIC R1, R1, #(3 << 26)
-    // Store content in r1 at address found in r0
+    // Almacenar el contenido en r1 en la dirección encontrada en r0
     str r1, [r0]
    // bl check_button
     b off
@@ -217,6 +261,6 @@ read_button:
 
 
 .section .data
-time_delay_1_5_seconds: .word 5000000  // Approximate delay count for 1.5 seconds
+time_delay_1_5_seconds: .word 5000000  // Conteo aproximado de retraso para 1.5 segundos
 .align
 .end
